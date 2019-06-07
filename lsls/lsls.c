32425fbcd4ef2ed00915468740b3d2a869e53fea
matt0418 <mfeldma7@gmail.com>
2019-06-07 11:58:43 -0400

    making some progress

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,14 +4,34 @@
 /**
  * Main
  */
+
 int main(int argc, char **argv)
 {
-  // Parse command line
+  DIR *dir;
+  struct dirent *dp;
+  char * file_name;
+  printf("There are %d command line argument(s):\n", argc);
+  printf("I want to use this %s\n", argv[1]);
+
 
   // Open directory
+  if (argv[1] == NULL) {
+    printf("Beofre I do antthing\n");
+    if ((dir = opendir(".")) == NULL) {
+      perror("Can't open directory");
+      exit(1);
+    } else {
+        dp = readdir(".");
+        while (&dp != NULL) {
+          printf("I am here");
+        
+        
+      }
+      closedir(dir);
+    }
+  }
 
   // Repeatly read and print entries
-
   // Close directory
 
   return 0;
