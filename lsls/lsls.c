b3a2e7afea587b38dcd584cd21f0cb133101221e
matt0418 <mfeldma7@gmail.com>
2019-06-07 12:32:58 -0400

    completed the coding part

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <dirent.h>
-
+#include <libgen.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
 /**
  * Main
  */
@@ -9,27 +13,32 @@ int main(int argc, char **argv)
 {
   DIR *dir;
   struct dirent *dp;
-  char * file_name;
   printf("There are %d command line argument(s):\n", argc);
   printf("I want to use this %s\n", argv[1]);
 
-
   // Open directory
   if (argv[1] == NULL) {
-    printf("Beofre I do antthing\n");
-    if ((dir = opendir(".")) == NULL) {
-      perror("Can't open directory");
-      exit(1);
-    } else {
-        dp = readdir(".");
-        while (&dp != NULL) {
-          printf("I am here");
-        
-        
-      }
-      closedir(dir);
-    }
+    argv[1] = ".";
   }
+
+  if ((dir = opendir (argv[1])) == NULL) {
+    perror ("Cannot open");
+    exit (1);
+  }
+
+  while ((dp = readdir(dir)) != NULL) {
+    struct stat st;
+    stat(dp->d_name, &st);
+    if (st.st_mode & S_IFDIR) {
+      printf(" <DIR> ");
+    } else {
+      int size = st.st_size;
+      printf("File Size == %d bytes\n", size);
+    }
+
+    printf("%s\n", dp->d_name);
+  }
+  closedir(dir);
 
   // Repeatly read and print entries
   // Close directory
